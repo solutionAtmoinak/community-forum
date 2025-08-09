dff78f19aaaa42b9ef6c1632b6b8f587dae14475
solutionAtmoinak <moinak.solution@gamil.com>
2025-08-09 16:09:40 +0530

    remove next font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
 import type { Metadata } from "next";
 import { ToastContainer } from "react-toastify";
-import { Poppins } from 'next/font/google'
 
-const poppins = Poppins({ weight: ['400', '500', '600', '700', '800'], display: 'swap', fallback: ['sans'] })
+
+
 
 
 export const metadata: Metadata = {
@@ -24,7 +24,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        style={poppins.style}
         className={`antialiased mb-[80px] md:mb-0`}
       >
         <Navbar />
